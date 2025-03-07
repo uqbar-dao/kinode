6abd05767f0be3877d27a5b31a623dd65c370eda
hosted-fornet <hosted-fornet@protonmail.com>
2025-03-07 00:42:27 -0800

    move to mainnet

--- hyperdrive/src/register-ui/src/abis/index.ts
+++ hyperdrive/src/register-ui/src/abis/index.ts
@@ -3,10 +3,10 @@ import { parseAbi } from "viem";
 export { generateNetworkingKeys } from "./helpers";
 
 // move to constants? // also for anvil/base
-export const HYPERMAP: `0x${string}` = "0x000000000033e5CCbC52Ec7BDa87dB768f9aA93F";
+export const HYPERMAP: `0x${string}` = "0x000000000044C6B8Cb4d8f0F889a3E47664EAeda";
 export const MULTICALL: `0x${string}` = "0xcA11bde05977b3631167028862bE2a173976CA11";
-export const KINO_ACCOUNT_IMPL: `0x${string}` = "0x000000000012d439e33aAD99149d52A5c6f980Dc";
-export const DOTOS: `0x${string}` = "0x091684B4C62DE83b864Db9591600f3751C2025c4";
+export const HYPER_ACCOUNT_IMPL: `0x${string}` = "0x0000000000EDAd72076CBe7b9Cfa3751D5a85C97";
+export const DOTOS: `0x${string}` = "0x763Ae1AB24c4322b8933E58d76d8D9286f6C0162";
 
 export const multicallAbi = parseAbi([
     `function aggregate(Call[] calls) external payable returns (uint256 blockNumber, bytes[] returnData)`,
